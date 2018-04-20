e65ca9e3b6b49e7e8822ef22d0def56b1da68f9f
Frank Dawes <jdawes22@gmail.com>
2018-04-20 15:43:59 +0100

    access allowed for put requests

--- app.js
+++ app.js
@@ -11,9 +11,13 @@ app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
     next();
-  });
+   });
 
 mongoose.connect(DB_URL)
     .then(() => {
